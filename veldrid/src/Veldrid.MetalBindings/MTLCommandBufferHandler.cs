29f2d51faf51d337e3ea7b23d5633de8537eaebd
pavel.sverdlov <pavel.sverdlov@digitallyinspired.com>
2018-11-28 22:33:00 +0200

    cleanup

--- veldrid/src/Veldrid.MetalBindings/MTLCommandBufferHandler.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace Veldrid.MetalBindings
-{
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void MTLCommandBufferHandler(MTLCommandBuffer buffer);
-}
--- veldrid/src/Veldrid/BindableResource.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Veldrid
-{
-    /// <summary>
-    /// A resource which can be bound in a <see cref="ResourceSet"/> and used in a shader.
-    /// See <see cref="DeviceBuffer"/>, <see cref="TextureView"/> and <see cref="Sampler"/>.
-    /// </summary>
-    public interface BindableResource { }
-}
