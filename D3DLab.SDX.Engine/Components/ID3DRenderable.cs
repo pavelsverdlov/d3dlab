ce5ac31aed4e90b4a3c7fc2050e83a58df177532
Pavel Sverdlov <sverdlov@gmal.com>
2020-05-25 20:54:22 +0300

    Cleanup

--- D3DLab.SDX.Engine/Components/ID3DRenderable.cs
+++
@@ -1,8 +0,0 @@
-﻿using D3DLab.ECS;
-
-namespace D3DLab.SDX.Engine.Components {
-    public interface ID3DRenderable : IGraphicComponent {
-        void Update(GraphicsDevice graphics);
-        void Render(GraphicsDevice graphics);
-    }
-}
